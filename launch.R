dab9b8f00f6a9ba02045ef17bb975033c9ffd84c
AJ <dataparadigms@gmail.com>
2013-03-02 13:27:51 -0500

    starts the app

---
+++ launch.R
@@ -0,0 +1,14 @@
+# file: launch.R
+# 
+# author: AJ
+# twitter: dataparadigms
+# web: http://www.dataparadigms.com
+#
+# description:
+#  loads the shiny library and launches the app assumes the working directory 
+#  has been set to the directory where this file is
+
+library(shiny)
+runApp(getwd())
+
+

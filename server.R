2b57be1011cbfb5d5ec0a870bd1d508dfe4d149f
AJ <dataparadigms@gmail.com>
2013-03-03 16:34:22 -0500

    commit for user functions to load data and assign colors

--- server.R
+++ server.R
@@ -1,4 +1,8 @@
 # file: server.R
+# 
+# author: AJ
+# twitter: dataparadigms
+# web: http://www.dataparadigms.com
 #
 # description:
 #  defines the various server logic needed 
@@ -9,54 +13,85 @@ library(ggplot2)
 library(scales)
 library(RColorBrewer)
 
-# do the nessecary data mungling,  this step will take a bit to initialize
-stream <- read.csv("http://www.procrun.com/popeOdds/output.csv",
+loadData <- function(){
+  # loads the most recent odds, on load and then when user chooses to. 
+  #
+  # returns:
+  #   munged data.frame needed for the server
+
+  # temporary server for data file to prevent overflow on static site
+
+  stream <- read.csv("http://www.procrun.com/popeOdds/output.csv",
                    as.is = TRUE,
                    header = FALSE)
-colnames(stream) <- c("x", "title", "name", "country", "odds", "y")
+  colnames(stream) <- c("x", "title", "name", "country", "odds", "y")
 
-# put together a new data frame to export to json
-condensed <- data.frame(candidate = paste(stream$title, 
-                                          stream$name, 
-                                          paste("(", stream$country, ")", sep="")),
-                        x = parse_date_time(stream$x, '%Y%m%d%H%M%S'), 
-                        y = stream$y)
+  # put together a new data frame
+  condensed <- data.frame(candidate = paste(stream$title, 
+                                            stream$name, 
+                                            paste("(", stream$country, ")", sep="")),
+                          x = parse_date_time(stream$x, '%Y%m%d%H%M%S'), 
+                          y = stream$y)
 
-# check for any duplicates
-condensed <- condensed[!duplicated(condensed), ]
+  # check for any duplicates
+  condensed <- condensed[!duplicated(condensed), ]
 
-# order
-condensed <- condensed[with(condensed, order(candidate, x)),]
+  # order
+  condensed <- condensed[with(condensed, order(candidate, x)),]
 
-# rank for the max time
-ranked <- condensed[as.double(condensed$x) == as.double(max(condensed$x)), ]
-ranked$rank <- rank(-ranked$y, ties.method = "min")
-ranked <- ranked[with(ranked, order(rank)),]
-ranked <- ranked[ranked$rank <= 10, ]
+  # rank for the max time
+  ranked <- condensed[as.double(condensed$x) == as.double(max(condensed$x)), ]
+  ranked$rank <- rank(-ranked$y, ties.method = "min")
+  ranked <- ranked[with(ranked, order(rank)),]
+  ranked <- ranked[ranked$rank <= 10, ]
 
-# merge back
-condensed <- merge(condensed, ranked[, c(1,4)], by = c("candidate"))
-candidateColors <- brewer.pal(12, "Paired")
-names(candidateColors) <- levels(factor(condensed$candidate))
+  # merge back
+  condensed <- merge(condensed, ranked[, c(1,4)], by = c("candidate"))
+  return(condensed)
+}
 
+setColors <- function(df){
+  # sets consistent colors for the plot based on the data
+  #
+  # args:
+  #   df: data.frame with a list of candidates and no more than 12 for
+  #       for unique color assignment
+  #
+  # returns:
+  #   vector of colors and corresponding name
+
+  colors <- rev(brewer.pal(10, "Spectral"))
+  names(colors) <- levels(factor(df$candidate))
+  
+  return(colors)
+}
+
+
+# sets up based on the data when the server was loaded
+options <- loadData()
+candidateColors <- setColors(options)
+
+# needed for the shiny server to run
 shinyServer(function(input, output) {
 
   # reactive to the slide to pull the the top n ranks
-  sliderValues <- reactive({
-    temp <- subset(condensed, rank <= input$rank)
+  sliderValues <- reactive({    
+    temp <- subset(options, rank <= input$rank)
   })
 
-  # show the values using an html table
+  # updates the data based on user check box
+  #
+  # TODO
 
+  # render plot to pass the ggplot back to the ui.R
   output$ggplot <- renderPlot({
-
     # set stable temp df so the max function doesn't force a function recall
     temp <- sliderValues()
 
     # create the plot
     p <- ggplot(temp, aes(x, y))
 
-    p <- p + geom_line(aes(colour = candidate)) +
+    p <- p + geom_line(aes(colour = candidate), size = 1.25) +
           theme_bw() +
           scale_y_continuous(breaks = seq(0,1,.05)) +
           scale_color_manual(name = "candidate", values = candidateColors) + 
--- ui.R
+++ ui.R
@@ -1,12 +1,16 @@
 # file: ui.R
-#
+# 
+# author: AJ
+# twitter: dataparadigms
+# web: http://www.dataparadigms.com
 #
 # description:
-#  sets the user interfact
+#  sets the user interface
 
 library(shiny)
 
-# Define UI for miles per gallon application
+# define the UI set up, bootstrapPage lets the span for the graph be 12 instead
+# of the default 8 with pageWithSidebar
 shinyUI(bootstrapPage(
 
   headerPanel("Next Pope"),
@@ -14,34 +18,16 @@ shinyUI(bootstrapPage(
   mainPanel(
 
     div(class="span12",
-      plotOutput("ggplot")),
+      plotOutput("ggplot")
+    ),
 
     div(class="span2",
       sliderInput("rank", 
       "Top N Candidates:",
       min = 1,
       max = 10,
-      value = 1))
+      value = 1)
+    )
   )
 
 ))
-
-
-# <div class="navbar navbar-inverse navbar-fixed-top">
-#   <div class="navbar-inner">
-#     <div class="container">
-#       <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-#         <span class="icon-bar"></span>
-#         <span class="icon-bar"></span>
-#         <span class="icon-bar"></span>
-#       </a>
-#       <a class="brand" href="index.html">Everlasting Classic</a>
-#       <div class="nav-collapse collapse">
-#         <ul class="nav">
-#           <li class="active"><a href="index.html">Home</a></li>
-#           <li><a href="http://everlastingclassic.com/blog/">Blog</a></li>
-#         </ul>
-#       </div><!--/.nav-collapse -->
-#     </div>
-#   </div>
-# </div>
